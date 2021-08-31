11d9a157c8e94bb955b3fe688bc07aa59c2be41f
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-08-31 17:17:23 +0530

    modified functions of ipl.js and adding edge cases

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,14 +1,7 @@
 // Function for count the Number of matches played per year for all the years in IPL.
 function numberOfMatchesPerYear(matches) {
     let result = {};
-    matches.forEach(element => {
-        if (element.season in result){
-            result[element.season] += 1;
-        }
-        else{
-            result[element.season] = 1;
-        }
-    });
+    matches.forEach(element => element.season in result ? result[element.season] += 1 : result[element.season] = 1);
     return result;
 }
 
@@ -17,19 +10,13 @@ function numberOfMatchesWonPerTeamPerYear(matches) {
     let result = {};
     let matchesWonPerTeam = {};
     matches.forEach(element => {
-        if (element.season in result){
-            if (element.winner in matchesWonPerTeam){
-                matchesWonPerTeam[element.winner] += 1;
-            }
-            else{
-                matchesWonPerTeam[element.winner] = 1;
-            }
+        element.season in result ? 
+        element.winner in matchesWonPerTeam ? matchesWonPerTeam[element.winner] += 1
+        : element.winner ? matchesWonPerTeam[element.winner] = 1
+        : 
+        result[element.season] = matchesWonPerTeam
+        : matchesWonPerTeam = {};
             result[element.season] = matchesWonPerTeam;
-        }
-        else{
-            matchesWonPerTeam = {};
-            result[element.season] = matchesWonPerTeam;
-        }
     });
     return result;
 }
@@ -37,61 +24,46 @@ function numberOfMatchesWonPerTeamPerYear(matches) {
 //Function for count the Extra runs conceded per team in the year 2016
 function extraRunConcededPerTeamIn2016(matches, deliveries){
     let result = {};
-    const matchesIn2016 = matches.filter(element => element.season == 2016);
-    matchesIn2016.forEach(element2016 => {
+    matches.filter(element => element.season == 2016)
+    .forEach(element2016 =>{
         deliveries.forEach(element => {
-            if (element.match_id == element2016.id){
-                if (element.bowling_team in result){
-                    result[element.bowling_team] += Number(element.extra_runs);
-                }
-                else{
-                    result[element.bowling_team] = Number(element.extra_runs);
-                }
-            }
-        });
-    });
+            element.match_id == element2016.id ? element.bowling_team in result ? result[element.bowling_team] += Number(element.extra_runs)
+            : result[element.bowling_team] = Number(element.extra_runs)
+            : {};
+        })
+    })
     return result;
 }
 
 //Function for get the Top 10 economical bowlers in the year 2015
 function top10EconomicalBowlersIn2015(matches, deliveries){
-    let result = [];
+    let result = {};
     let bowlersRuns = {};
     let bowlersNumberOfDeliveries = {};
     let count = 0;
-    const matchesIn2015 = matches.filter(element => element.season == 2015);
-    matchesIn2015.forEach(element2015 => {
+    matches.filter(element => element.season == 2015)
+    .forEach(element2015 => {
         deliveries.forEach(element => {
             if (element.match_id == element2015.id){
-                if (element.bowler in bowlersRuns){
-                    bowlersRuns[element.bowler] += element.total_runs;
-                }
-                else{
-                    bowlersRuns[element.bowler] = element.total_runs;
-                }
-                if (element.bowler in bowlersNumberOfDeliveries && !(element.wide_runs) && !(element.noball_runs)){
-                    bowlersNumberOfDeliveries[element.bowler] += 1;
-                }
-                else if (!(element.wide_runs) && !(element.noball_runs)){
-                    bowlersNumberOfDeliveries[element.bowler] = 1;
-                }
+                element.bowler in bowlersRuns ? bowlersRuns[element.bowler] += Number(element.total_runs) 
+                : bowlersRuns[element.bowler] = Number(element.total_runs)
+                element.bowler in bowlersNumberOfDeliveries && element.wide_runs == 0 && element.noball_runs == 0 ? bowlersNumberOfDeliveries[element.bowler] += 1
+                : element.wide_runs == 0 && element.noball_runs == 0 ? bowlersNumberOfDeliveries[element.bowler] = 1
+                : {};
             }
         });
     });
     Object.keys(bowlersRuns).map(key => {
-        bowlersRuns[key] = (bowlersRuns[key] / (bowlersNumberOfDeliveries[key] / 6).toFixed(2)).toFixed(2); // (bowlersNumberOfDeliveries[key] / 6) calculate number of overs
-    });
-    let bowlersEconomy = Object.entries(bowlersRuns).sort((a,b) => a[1]-b[1]);
-    bowlersEconomy.forEach(element => {
-        if (count <= 10){
-            result.push(element[0]);
+        bowlersRuns[key] = (bowlersRuns[key] / (bowlersNumberOfDeliveries[key] / 6)).toFixed(2); // (bowlersNumberOfDeliveries[key] / 6) calculate number of overs
+    })
+    Object.entries(bowlersRuns).sort((a,b) => a[1]-b[1])
+    .forEach(element => {
+        if (count < 10){
+            result[element[0]] = element[1];
             count += 1;
         }
-    });
+    })
     return result;
 }
 
-module.exports.numberOfMatchesPerYear = numberOfMatchesPerYear;
-module.exports.numberOfMatchesWonPerTeamPerYear = numberOfMatchesWonPerTeamPerYear;
-module.exports.extraRunConcededPerTeamIn2016 = extraRunConcededPerTeamIn2016;
-module.exports.top10EconomicalBowlersIn2015 = top10EconomicalBowlersIn2015;
+module.exports = {numberOfMatchesPerYear, numberOfMatchesWonPerTeamPerYear, extraRunConcededPerTeamIn2016, top10EconomicalBowlersIn2015};
