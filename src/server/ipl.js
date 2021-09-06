3c608315a1d401d37245eb5207ec51af851dca60
Abhishek Yadav <abhishek.yadav.17@mountblue.tech>
2021-09-06 15:37:57 +0530

    replacing forEach to reduce function

--- src/server/ipl.js
+++ src/server/ipl.js
@@ -1,7 +1,9 @@
 // Function for count the Number of matches played per year for all the years in IPL.
 function numberOfMatchesPerYear(matches) {
     let result = {};
-    matches.forEach(element => element.season in result ? result[element.season] += 1 : result[element.season] = 1);
+    matches.reduce((matchPerYear, {season}) => {
+        season in result ? result[season] += 1 : result[season] = 1;
+    });
     return result;
 }
 
@@ -9,14 +11,14 @@ function numberOfMatchesPerYear(matches) {
 function numberOfMatchesWonPerTeamPerYear(matches) {
     let result = {};
     let matchesWonPerTeam = {};
-    matches.forEach(element => {
-        element.season in result ? 
-        element.winner in matchesWonPerTeam ? matchesWonPerTeam[element.winner] += 1
-        : element.winner ? matchesWonPerTeam[element.winner] = 1
+    matches.reduce((matchPerYear, {season, winner}) => {
+        season in result ? 
+        winner in matchesWonPerTeam ? matchesWonPerTeam[winner] += 1
+        : winner ? matchesWonPerTeam[winner] = 1
         : 
-        result[element.season] = matchesWonPerTeam
+        result[season] = matchesWonPerTeam
         : matchesWonPerTeam = {};
-            result[element.season] = matchesWonPerTeam;
+        result[season] = matchesWonPerTeam;
     });
     return result;
 }
@@ -24,14 +26,14 @@ function numberOfMatchesWonPerTeamPerYear(matches) {
 //Function for count the Extra runs conceded per team in the year 2016
 function extraRunConcededPerTeamIn2016(matches, deliveries){
     let result = {};
-    matches.filter(element => element.season == 2016)
-    .forEach(element2016 =>{
-        deliveries.forEach(element => {
-            element.match_id == element2016.id ? element.bowling_team in result ? result[element.bowling_team] += Number(element.extra_runs)
-            : result[element.bowling_team] = Number(element.extra_runs)
-            : {};
-        })
-    })
+    matches.filter(({season}) => season == 2016)
+    .reduce((match2016, {id}) => {
+        deliveries.reduce((delivery, {match_id, extra_runs, bowling_team}) => {
+           match_id == id ? bowling_team in result ? result[bowling_team] += Number(extra_runs)
+            : result[bowling_team] = Number(extra_runs)
+            : {}; 
+        });
+    });
     return result;
 }
 
@@ -41,21 +43,23 @@ function top10EconomicalBowlersIn2015(matches, deliveries){
     let bowlersRuns = {};
     let bowlersNumberOfDeliveries = {};
     let count = 0;
-    matches.filter(element => element.season == 2015)
-    .forEach(element2015 => {
-        deliveries.forEach(element => {
-            if (element.match_id == element2015.id){
-                element.bowler in bowlersRuns ? bowlersRuns[element.bowler] += Number(element.total_runs) 
-                : bowlersRuns[element.bowler] = Number(element.total_runs)
-                element.bowler in bowlersNumberOfDeliveries && element.wide_runs == 0 && element.noball_runs == 0 ? bowlersNumberOfDeliveries[element.bowler] += 1
-                : element.wide_runs == 0 && element.noball_runs == 0 ? bowlersNumberOfDeliveries[element.bowler] = 1
+    matches.filter(({season}) => season == 2015)
+    .reduce((match2016, {id}) => {
+        deliveries.reduce((delivery, {match_id, bowler, total_runs, wide_runs, noball_runs}) => {
+            if (match_id == id){
+                bowler in bowlersRuns ? bowlersRuns[bowler] += Number(total_runs) 
+                : bowlersRuns[bowler] = Number(total_runs)
+                bowler in bowlersNumberOfDeliveries && wide_runs == 0 && noball_runs == 0 ? bowlersNumberOfDeliveries[bowler] += 1
+                : wide_runs == 0 && noball_runs == 0 ? bowlersNumberOfDeliveries[bowler] = 1
                 : {};
             }
-        });
-    });
+        })
+    })
+
     Object.keys(bowlersRuns).map(key => {
         bowlersRuns[key] = (bowlersRuns[key] / (bowlersNumberOfDeliveries[key] / 6)).toFixed(2); // (bowlersNumberOfDeliveries[key] / 6) calculate number of overs
     })
+    
     Object.entries(bowlersRuns).sort((a,b) => a[1]-b[1])
     .forEach(element => {
         if (count < 10){
